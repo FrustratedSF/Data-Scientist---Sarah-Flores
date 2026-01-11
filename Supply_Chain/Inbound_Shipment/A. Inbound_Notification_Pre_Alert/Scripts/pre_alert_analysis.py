90eebdc52ef512169f27887bf64af56cca149ffe
FrustratedSF <frustratedgeek94@gmail.com>
2026-01-11 22:14:36 +0100

    Update pre_alert_analysis.py

--- Supply_Chain/Inbound_Shipment/A. Inbound_Notification_Pre_Alert/Scripts/pre_alert_analysis.py
+++ Supply_Chain/Inbound_Shipment/A. Inbound_Notification_Pre_Alert/Scripts/pre_alert_analysis.py
@@ -4,3 +4,14 @@ import pandas as pd
 df = pd.read_csv('../Data/Pre-Alert.csv')
 
 print(df.head())
+
+# Understand the data
+# Before analyzing the data, we need to consider things:
+# A. What columns do we have? | B. What data types?  | C. Are there missing values? | D. How big is the dataset?
+
+# A. Check the column names (Column inspection)
+print('Columns: ')
+print(df.columns)
+
+
+# to be continues
